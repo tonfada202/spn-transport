3faaab7f445da069526fdced996465283bd9673b
Autsadakorn Jarernket <autsadakornjarernket@Autsadakorns-MacBook-Pro.local>
2018-09-09 19:19:06 +0700

    first commit

---
+++ src/assets/js/creative.js
@@ -0,0 +1,23 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+
+
+})(jQuery); // End of use strict
